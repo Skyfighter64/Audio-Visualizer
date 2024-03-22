7d7f903568f17deffb42fd2f6e6e709fb525cdfc
Skyfighter64 <skyfighter64@protonmail.com>
2024-03-22 01:23:33 +0100

    working on auto configuration

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-hello world
+# Audio-Visualizer
+An audio visualizer for my Raspberry Pi which combines Addressable LEDs and the [CAVA Audio Visualizer](https://github.com/karlstav/cava) by using an Arduino with [ALUP](https://github.com/Skyfighter64/ALUP) and a custom ALSA Configuration.
+
+# Requirements
+- Raspberry Pi to play Audio (any other linux system will probably work)
+- [CAVA Audio Visualizer](https://github.com/karlstav/cava)
+- Audio Loopback device enabled (usually via `sudo modprobe snd-aloop`)
+- Arduino connected via USB and [Arduino-ALUP](https://github.com/Skyfighter64/Arduino-ALUP) configured and running
+
+# Install
+Use this to clone the repo and also clone the [Python-ALUP](https://github.com/Skyfighter64/Python-ALUP) submodule
+`git clone --recurse-submodules https://github.com/Skyfighter64/Audio-Visualizer`
+or
+`git clone https://github.com/Skyfighter64/Audio-Visualizer`
+`cd Audio-Visualizer`
+`git submodule update --init`
+
+# Configuration
+todo
+also todo: make setup guide to set up alsa config, soundcards, loopback devices and custom cava configs 
--- alup_cava_translator.py
+++ alup_cava_translator.py
@@ -1,7 +1,9 @@
 import subprocess
 import time
 import os
+import shutil
 from pathlib import Path
+import tempfile
 # import the main library
 from Python-ALUP.src.Device import Device
 # import command definitions
@@ -16,7 +18,6 @@ from Python-ALUP.src.Frame import Frame
 # 1. Connect to ALUP, get config data
 # 1.5 create temporary fifo file 
 # 2. Create custom cava config with bars = num_leds
-
 # 3. Launch cava instance with custom config and output to fifo file
 # 4. read cava binary data from fifo
 # 5. partition data to led brightness and send data to alup
@@ -30,9 +31,8 @@ from Python-ALUP.src.Frame import Frame
 
 # also see: https://github.com/karlstav/cava/issues/123#issuecomment-307891020
 
-# path to the cava fifo file
-# todo: replace with os tmp folder
-fifo_path = "/tmp/cava/fifo"
+# path to the cava tmp folder
+TMP_DIRECTORY = tempfile.gettempdir() + "/cava"
 # COM Port of the arduino
 COM_PORT = "COM9"
 BAUD_RATE = 115200
@@ -48,10 +48,16 @@ arduino = Device()
 def main():
     # Connect to ALUP
     arduino.SerialConnect(COM_PORT, BAUD_RATE)
+
+    # create tmp folder if non-existent
+    Path(folder).mkdir(parents=False, exist_ok=True)
+    # clear tmp folder 
+    #ClearDirectory(TMP_DIRECTORY) # temporarily disabled to do rm -rf concerns (high risks)
     # create temporary fifo
-    fifo_path = CreateFifo()
+    fifo_path = CreateFifo(TMP_DIRECTORY)
+
     # Create custom config 
-    config_path = CreateCavaConfig(arduino.configuration.ledCount, fifo_path)
+    config_path = CreateCavaConfig(arduino.configuration.ledCount, TMP_DIRECTORY)
 
     # Start Cava with created config
     cava_process = subprocess.Popen(["cava","-p", config_path])
@@ -70,23 +76,60 @@ def main():
             # send led frame
             arduino.Send()
 
+
+# remove all contents in directory recursively
+def ClearDirectory(folder):
+    for filename in os.listdir(folder):
+        file_path = os.path.join(folder, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print('Failed to delete %s. Reason: %s' % (file_path, e))
     
-    
-def CreateFifo(fifo_path):   
-    # create temp folder if non-existent
-    Path(os.path.dirname(os.path.abspath(fifo_path))).mkdir(parents=False, exist_ok=True)
-    
+def CreateFifo(folder):    
+    fifo_path = folder + "/fifo"
     # create fifo file if not exists
     if not os.path.exists(fifo_path):
         os.mkfifo(fifo_path)
-        
+
     return fifo_path
 
 
-def CreateCavaConfig(num_leds):
-    # read in default config
+# replace the first occurence of the matching regex with the contents of replacement_line
+# in the given array of strings or appends it to the end if no match is found
+# returns the number of replaced lines
+def ReplaceLine(lines, regex, replacement_line):
+    pattern = re.compile(regex, re.MULTILINE)
+    occurences = 0
+    for line in lines:
+        if pattern.match(line):
+            line = replacement_line
+            occurences += 1
+    
+    print("Replaced " + str(occurences) + " line(s) with \"" + replacement_line + "\"")
+
+    if(occurences is 0):
+        lines.append(replacement_line)
+        print("No match found for regex \"" + str(regex) + "\". Appended line \"" + replacement_line + "\" at the end of file")
+
+
+def CreateCavaConfig(num_leds, folder):
+    default_config_path = Path(__file__).parent.resolve() + "cava_config"
+    temp_config_file_name = "cava_tmp_config"
+    # read default config
+    with open(default_config_path, "r") as file:
+        config_lines = file.readlines()
     # change bars to num_leds
+    ReplaceLine(config_lines, regex = r"^ ?;? ?bars ?= ?[0-9]+ ?$", replacement_line = "bars = " + str(num_leds))
     # make sure bit_format is 8 bit
+    ReplaceLine(config_lines, regex = r"^ ?;? ?sample_bits ?= ?[0-9]+ ?$", replacement_line = "sample_bits = 8")
+    # make sure mode is raw
+    # make sure data format is binary
+    # set fifo target file to fifo path
+
     # paste into new custom config to temp folder
     # return path of custom config 
 
